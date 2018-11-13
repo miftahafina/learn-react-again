21981ffa8fde6b09116b4dfbc0c2783baf3667f1
Miftah Afina <miftah.afina@gmail.com>
2018-11-13 16:48:46 +0700

    Props

--- App.js
+++ App.js
@@ -1,31 +1,28 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import PresentationalComponent from './PresentationalComponent';
 
 export default class App extends React.Component {
   state = {
-    myState: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, used do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    myState: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, used do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    myState2: 'Haiiahh'
   }
 
   updateState = () => {
     return this.setState({
-      myState: 'The state is udpated'
+      myState: 'The state is updated'
     });
   }
+
+  updateState2 = () => this.setState({
+    myState2: 'This state is also updated'
+  });
 
   render() {
     return (
       <View>
-        <Text onPress = {this.updateState}>
-          {this.state.myState}
-        </Text>
+        <PresentationalComponent myState = {this.state.myState} updateState = {this.updateState} />
+        <PresentationalComponent myState = {this.state.myState2} updateState = {this.updateState2} />
       </View>
     );
   }
