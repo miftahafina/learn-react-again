3a56853864778945d4746974a41ac6f7493a1784
Miftah Afina <miftah.afina@gmail.com>
2018-11-13 15:03:48 +0700

    Update state

--- App.js
+++ App.js
@@ -10,10 +10,22 @@ import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
 export default class App extends React.Component {
+  state = {
+    myState: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, used do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  }
+
+  updateState = () => {
+    return this.setState({
+      myState: 'The state is udpated'
+    });
+  }
+
   render() {
     return (
       <View>
-        <Text>Welcome to Tutorialspoint</Text>
+        <Text onPress = {this.updateState}>
+          {this.state.myState}
+        </Text>
       </View>
     );
   }
