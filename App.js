649fc029e7a4c07f371fe10af9fce1dd342de32e
Miftah Afina <miftah.afina@gmail.com>
2018-11-13 14:44:29 +0700

    Welcome to tutorialspoint

---
+++ App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Welcome to Tutorialspoint</Text>
+      </View>
+    );
+  }
+}
