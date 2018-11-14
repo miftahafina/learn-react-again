a05687a4212bfa7c3172788f84fb025de97692fe
Miftah Afina <miftah.afina@gmail.com>
2018-11-14 09:03:16 +0700

    Using fetch() to fetch data from API

--- App.js
+++ App.js
@@ -1,38 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import PresentationalComponent from './PresentationalComponent';
+import List from './List'
 
 export default class App extends React.Component {
-  state = {
-    myState1: 'State 1',
-    myState2: 'State 2',
-    myState3: 'State 3'
-  }
-
-  updateState1 = () => {
-    return this.setState({
-      myState1: 'State 1 updated'
-    });
-  }
-
-  updateState2 = () => this.setState({
-    myState2: 'State 2 updated'
-  });
-
-  updateState3 = () => this.setState({
-    myState3: 'State 3 updated'
-  });
-
-  // update
-
   render() {
     return (
-      <View>
-        {/* <PresentationalComponent myState = {this.state.myState1} updateState = {this.updateState1} />
-        <PresentationalComponent myState = {this.state.myState2} updateState = {this.updateState2} />
-        <PresentationalComponent myState = {this.state.myState3} updateState = {this.updateState3} /> */}
-        <PresentationalComponent />
-      </View>
+      <List />
     );
   }
 }
---
+++ List.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { FlatList, ActivityIndicator, Text, TouchableOpacity, View, StyleSheet } from "react-native";
+
+export default class List extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      data: []
+    }
+  }
+  
+  updateState = () => {
+    return this.setState({
+      isLoading: !this.state.isLoading
+    })
+  }
+
+  componentDidMount = () => {
+    fetch('http://miftahafina.klik.press/api/person', {
+      method: 'GET'
+    })
+    .then((response) => response.json())
+    .then((responseJson) => {
+      console.log(responseJson);
+      this.setState({
+        data: responseJson,
+        isLoading: false
+      })
+    })
+    .catch((error) => {
+      console.error(error)
+    });
+  }
+
+  alertCellNumber = (item) => {
+    alert(item.cell_number)
+  }
+
+  render () {
+    if (this.state.isLoading) {
+      return(
+        <TouchableOpacity style = {styles.default} onPress = {this.updateState}>
+          <ActivityIndicator />
+        </TouchableOpacity>
+      )
+    }
+
+    return (
+      <View>
+        {
+          this.state.data.map((item, index) => (
+            <TouchableOpacity style = {styles.default} onPress = {() => this.alertCellNumber(item)} key = {item.id_person}>
+              <Text>{item.name}</Text>
+            </TouchableOpacity>
+          ))
+        }
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  default: {
+    flex: 1,
+    padding: 20
+  }
+});
